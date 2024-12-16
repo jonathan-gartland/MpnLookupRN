59f4948231d0983412f7fad587c754a0ff2065e6
jonathan-gartland <jonathan-gartland@gmail.com>
2024-12-16 15:49:01 -0500

    chore: back to refactoring web tests for the app

--- __tests__/components/button.view.test.tsx
+++ __tests__/components/button.view.test.tsx
@@ -3,7 +3,7 @@ import '@testing-library/jest-dom';
 import { render, screen, fireEvent } from '@testing-library/react-native';
 
 import ButtonView from '@/components/button-view';
-import { TouchableOpacity } from 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
 
 describe('Home', () => {
 
@@ -20,38 +20,36 @@ describe('Home', () => {
     const qt2kButton = screen.getByTestId('qt2k-button');
     const qtLButton = screen.getByTestId('qtl-button');
     const aboutButton = screen.getByTestId('about-button');
-    const onButtonPressMock = jest.fn();
-    expect(title).toBeDefined();
+    const onPressMock = jest.fn();
+
+    expect(title).toBeOnTheScreen();
     expect(title.props.children).toBe('MPN Lookup');
-
-    expect(qtButton).toBeDefined();
-    expect(qt2kButton).toBeDefined();
-    expect(qtLButton).toBeDefined();
-    expect(aboutButton).toBeDefined();
-
-    expect(1).toBe(1);
+    expect(qtButton).toBeOnTheScreen();
+    expect(qt2kButton).toBeOnTheScreen();
+    expect(qtLButton).toBeOnTheScreen();
+    expect(aboutButton).toBeOnTheScreen();
   });
 
   it('onPressItem is called when pressed', () => {
-    const onPressMock = jest.fn();
-    const { getByText, getByTestId } = render(<TouchableOpacity onPress={onPressMock}/>);
+    const onTouchableOpacityPressMock = jest.fn();
+    const { getByTestId } = render(
+      <TouchableOpacity
+        testID="about-button"
+        onPress={() => onTouchableOpacityPressMock('about-button')}
+      >
+        <Text>About</Text>
+      </TouchableOpacity>);
 
-    // find the TouchableOpacity and Text elements
-    //const buttonAbout = getByTestId('about-button');
-    //const text1 = getByText('About');
-
-
-    // fireEvent.press(text1);
-
-    // the PressMock handle is called with the 'id' of the item passed as an argument
-    // expect(onPressMock).toHaveBeenCalledWith('about-button');
+    fireEvent.press(getByTestId('about-button'));
+    expect(onTouchableOpacityPressMock).toHaveBeenCalledWith('about-button');
   });
-  //
-  // it("clicks the About button and verifies Have Fun! text", async () => {
-  //   fireEvent.click(screen.getByText("About"));
-  //   expect(await screen.findByText(/have fun!/i)).toBeInTheDocument();
-  // });
-  //
+
+
+  it("clicks the About button and verifies Have Fun! text", async () => {
+    fireEvent.press(screen.getByText("About"));
+    expect(await screen.findByText(/have fun!/i)).toBeOnTheScreen()
+  });
+
   // it("clicks the Quantitray&reg; button and verifies text", async () => {
   //   fireEvent.click(screen.getByTestId("qt-button"));
   //   expect(await screen.findByText(/Quantitray/i)).toBeInTheDocument();
